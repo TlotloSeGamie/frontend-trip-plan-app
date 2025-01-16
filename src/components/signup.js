b2e279bb0fa0172c75dfdddf05855970e0bddd84
Llewellyn <Llewellyn.ml.info@gmail.com>
2025-01-16 09:21:26 +0200

    travel

---
+++ src/components/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SignUp = () => {
+    return(
+        <div className="container">
+            Sign up page
+            </div>
+    )
+}
+
+export default SignUp
